0f7b621ebc435108c2355f2c22c16a51277a45dc
KovuriVaishnavi <22B81A6762@CVR.AC.IN>
2024-07-28 03:05:03 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 
+
+//import Header from "./components/header/Header";
 import './App.css';
 import Resource from './components/Resource/Resource';
 function App() {
-    const [username, setUsername] = useState(localStorage.getItem("username"));
-    const [position, setPosition] = useState(localStorage.getItem("position"));
 
   return (
     <>
