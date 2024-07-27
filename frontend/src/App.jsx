6071e20a7825e3f061de1dd8b7e3d0a4e901724e
Siri2512 <balabhadrasaisiri@gmail.com>
2024-07-27 22:47:35 +0530

    Creating frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Resource from './components/Resource/Resource'
+function App() {
+
+
+  return (
+    <>
+      <Navbar/>
+      <Resource/>
+    </>
+  )
+}
+
+export default App
